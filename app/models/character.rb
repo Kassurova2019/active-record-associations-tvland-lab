4bfa734137e8bc256bc68305f23526038e9e7e56
Luba Kassurova <kassurova@icloud.com>
2023-02-17 18:57:02 -0500

    commit tree ecba2ab2b56a58c3a72438102e7a6c7c6bcc3271

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,17 @@
+require "pry"
 class Character < ActiveRecord::Base
+  belongs_to :actor
+  belongs_to :show 
+
+  def say_that_thing_you_say
+    return "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(show_name)
+   new_show = Show.new(name: show_name[:name])
+   self.show = new_show
   
+  end
+    
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,23 @@
+require "pry"
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  def has_data_atributes
+    
+  end
+
+  def build_network(the_call_letters_hash)
+    #binding.pry
+new_network = Network.new(call_letters: the_call_letters_hash[:call_letters])
+self.network = new_network
+  end
+
+  def actors_list
+    self.actors.map do |actor| 
+        "#{actor.first_name} #{actor.last_name}"
+    end
+   # binding.pry
+  end
+
 end
